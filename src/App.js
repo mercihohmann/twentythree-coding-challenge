cbe4894dfc605f19e9bd42f56eeffbcd019d7b5f
Merci Hohmann <mercihohmann@Mercis-MacBook-Air.local>
2021-10-05 15:47:21 +0200

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import { Button } from "./Button";
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      counter: 0
+    }
+    this.onClick = this.onClick.bind(this);
+  }
+
+  onClick() {
+    this.setState({ counter: this.state.counter + 1 });
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <h1>{this.state.counter}</h1>
+        <Button text="Press Me" onClick={this.onClick} />
+      </div>
+    );
+  }
 }
 
 export default App;
